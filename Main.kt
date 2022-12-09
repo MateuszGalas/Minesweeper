e74d73d1eb12ff218e339c4cae64f0a9a2b8c605
Mateusz Galaś <mateuszgalas94@gmail.com>
2022-12-09 15:21:40 +0100

    Adding getAdjacent method, actualization of printBoard method

--- Main.kt
+++ Main.kt
@@ -30,21 +30,53 @@ class MineSweeper {
         }
     }
 
-/*    fun random(): Field {
-        val list = listOf<Field>(Field.MINE, Field.BLANK, Field.BLANK, Field.BLANK)
-        return list[Random.nextInt(4)]
-    }*/
-
     fun printBoard() {
         for (i in board.indices) {
             for (j in board[i].indices) {
+                val numberOfMinesAround = getAdjacent(i, j).count { it == Field.MINE }
+
                 when(board[i][j]) {
-                    Field.BLANK -> print(Field.BLANK.value)
+                    Field.BLANK -> print(
+                        if(numberOfMinesAround == 0) Field.BLANK.value
+                        else numberOfMinesAround
+                    )
                     Field.MINE -> print(Field.MINE.value)
                 }
             }
             println()
         }
+    }
+
+    private fun getAdjacent(x: Int, y: Int): ArrayList<Field> {
+
+        // Size of given 2d array
+        val n = board.lastIndex
+        val m = board[0].lastIndex
+
+        //println("$n and $m")
+
+        // Initialising a vector array where
+        // adjacent elements will be stored
+        val v = ArrayList<Field>()
+
+        // Checking for adjacent elements
+        // and adding them to array
+
+        // Deviation of row that gets adjusted
+        // according to the provided position
+        for (i in (if (x > 0) -1 else 0)..if (x < n) 1 else 0) {
+
+            // Deviation of the column that
+            // gets adjusted according to
+            // the provided position
+            for (j in (if (y > 0) -1 else 0)..if (y < m) 1 else 0) {
+                if (i != 0 || j != 0) {
+                    v.add(board[x + i][y + j])
+                }
+            }
+        }
+        // Returning the vector array
+        return v
     }
 }
 
@@ -52,3 +84,4 @@ fun main() {
     val mine = MineSweeper()
     mine.printBoard()
 }
+
