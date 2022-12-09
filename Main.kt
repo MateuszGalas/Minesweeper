5f4344f540283cc2f005c6bb4c0eca00f923f6b8
Mateusz Galaś <mateuszgalas94@gmail.com>
2022-12-09 12:23:09 +0100

    Adding number of mines on Board

--- Main.kt
+++ Main.kt
@@ -13,9 +13,29 @@ class MineSweeper {
     private val board = Array(BoardSize) { Array<Field>(BoardSize) { Field.BLANK } }
 
     init {
+        println("How many mines do you want on the field?")
+        val numberOfMines = readln().toInt()
+        val numberOfBlank = (BoardSize * BoardSize) - numberOfMines
+        var random: Field
+        var mine = 0
+        var blank = 0
+
+
         for (i in board.indices) {
-            for (j in board[i].indices) {
-                board[i][j] = random()
+            var j = 0
+
+            while (j < BoardSize) {
+                random = random()
+                if (random == Field.MINE && mine < numberOfMines) {
+                    board[i][j] = random
+                    mine += 1
+                } else if (random == Field.BLANK && blank < numberOfBlank) {
+                    board[i][j] = random
+                    blank += 1
+                } else {
+                    j--
+                }
+                j++
             }
         }
     }
